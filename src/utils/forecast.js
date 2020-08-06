2b1e9d4380d3ed1986dc214a6af7150af9a411c7
unknown <osmankozen@hotmail.com>
2020-08-06 15:21:10 +0300

    Add forecast response

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitute, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=6e04f7e0c9d9e9f684c5a9460dd488fa&query=' + latitude + ',' + longitute + '&units=f'
     
@@ -9,7 +11,7 @@ const forecast = (latitude, longitute, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.')
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} fah. Outside feelslike ${body.current.feelslike} fah.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} fah. Outside feelslike ${body.current.feelslike} fah. And Humidity is ${body.current.humidity}%.`)
         }
     })
 }
